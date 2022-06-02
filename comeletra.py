f0bd9a5a3463ab8ed172fc9fb58436e2f482a738
householdrat <orodriguez93@icloud.com>
2022-06-02 09:30:38 -0500

    codigo que come letras

---
+++ comeletra.py
@@ -0,0 +1,38 @@
+'''
+user_word = input("Ingresa una palabra: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+       print (letter)
+'''
+word_without_vowels = ""
+
+user_word = input("Ingresa una palabra: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    word_without_vowels += letter
+print (word_without_vowels)
+    
+
