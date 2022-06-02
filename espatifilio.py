351c2c58ffeaad28c9b5ebda86d53a173261d31e
householdrat <orodriguez93@icloud.com>
2022-06-02 09:31:06 -0500

    uso de input

---
+++ espatifilio.py
@@ -0,0 +1,10 @@
+planta = input("Ingresa el nombre de la planta que le gusta a python: ")
+
+if planta == "Espatifilio":
+    print("¡Espatifilio es la mejor planta de todas!")
+elif planta == "ESPATIFILIO":
+    print("SI, ¡EL ESPATIFILIO! es la mejor planta de todos los tiempos!")
+elif planta == "espatifilio":
+    print("No, ¡quiero un gran ESPATIFILIO!")
+else :
+    print ("¡ESPATIFILIO!, ¡No ", planta, "!")
