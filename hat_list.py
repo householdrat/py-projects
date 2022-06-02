694773d5539c85edb916ca69224e5a467c926eef
householdrat <orodriguez93@icloud.com>
2022-06-02 09:31:43 -0500

    modificacion de listas

---
+++ hat_list.py
@@ -0,0 +1,12 @@
+hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+# Paso 1: escribe una línea de código que solicite al usuario
+hat_list[2]= int(input("Ingresa un nuevo número: "))
+# reemplazar el número de en medio con un número entero ingresado por el usuario.
+print(hat_list)
+# Paso 2: escribe aquí una línea de código que elimine el último elemento de la lista.
+del hat_list [4]
+print(hat_list)
+# Paso 3: escribe aquí una línea de código que imprima la longitud de la lista existente.
+print(len(hat_list))
+print(hat_list)
